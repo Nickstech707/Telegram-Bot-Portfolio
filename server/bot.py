d751b0771388acaaf13c1fca07625f8b940caf89
Nicholas Muriithi <nickstech707@gmail.com>
2024-09-02 00:05:19 +0300

    Imported pdfplumber and define the function to extract text from a PDF

--- server/bot.py
+++ server/bot.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import joblib
+import pdfplumber
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 import google.generativeai as genai
@@ -17,3 +18,14 @@ MESSAGE_CHARACTER_LIMIT = 4096
 
 # Cache file path
 CACHE_FILE = "cache.joblib"
+
+
+
+# Extract text from the PDF
+def extract_pdf_text(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    print("PDF text extraction complete.")
+    return text
